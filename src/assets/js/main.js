6936d8da695e093a518a16641afa357f67b6ae33
Hassan Yousefi <hexboy2011@gmail.com>
2018-05-07 21:11:06 +0430

    update lodash example

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,4 @@
-var uniqueItems = _.chain([1, 2, 3, 4, 5, 6, 7, 4, 3, 100, 2, 1, 2, 4, 5, 6, 70, 7, 8, 9]).uniq().value();
+var uniqueItems = _([1, 2, 3, 4, 5, 6, 7, 4, 3, 100, 2, 1, 2, 4, 5, 6, 70, 7, 8, 9]).uniq();
 
 console.log(uniqueItems);
 
