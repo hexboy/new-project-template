02fc4ca5303cd8880d2c6d2c4dc7e5a47687bb01
Hassan Yousefi <hexboy2011@gmail.com>
2018-05-07 17:35:28 +0430

    update gulpfile and npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const fileinclude = require('gulp-file-include'),
 	gulp = require('gulp'),
-	watch = require('gulp-watch'),
 	clean = require('gulp-clean'),
 	runSequence = require('run-sequence'),
 	sourcemaps = require('gulp-sourcemaps'),
@@ -11,20 +10,21 @@ const fileinclude = require('gulp-file-include'),
 	cssmin = require('gulp-cssmin');
 
 gulp.task('clean', function() {
-		return gulp
+	return gulp
 		.src('./dist/*', { read: false })
 		.pipe(clean({ force: true }));
 });
 
 gulp.task('copy', function() {
-		return gulp
-			.src([
-				'./src/*',
-				'./src/**/{/js/**/*.min.js,/css/**/*.min.css}',
-				'./src/**/{/fonts/**/*,/img/**/*}',
-				'!./src/**/*.inc.html'
-			])
-			.pipe(gulp.dest('./dist'));
+	return gulp
+		.src([
+			'./src/*',
+			'./src/**/{/js/**/*.min.js,/css/**/*.min.css}',
+			'./src/**/{/fonts/**/*,/img/**/*}',
+			'!./src/**/*.inc.html',
+			'!./src/**/*.scss'
+		])
+		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('js', function() {
@@ -32,13 +32,13 @@ gulp.task('js', function() {
 		'./src/**/*.js',
 		'!./src/**/*.min.js'
 	])
-	.pipe(sourcemaps.init())
-	.pipe(babel({
-		presets: ['env']
-	}))
-	.pipe(concat('all.js'))
-	.pipe(sourcemaps.write('.'))
-	.pipe(gulp.dest('./dist/assets/js'));
+		.pipe(sourcemaps.init())
+		.pipe(babel({
+			presets: ['env']
+		}))
+		.pipe(concat('all.js'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist/assets/js'));
 });
 
 gulp.task('sass', function() {
@@ -48,22 +48,22 @@ gulp.task('sass', function() {
 });
 
 gulp.task('html', function() {
-		gulp.src([
-			'./src/*.html',
-			'!./src/*.inc.html'
-		])
+	gulp.src([
+		'./src/*.html',
+		'!./src/*.inc.html'
+	])
 		.pipe(fileinclude({
-				prefix: '@@',
-				basepath: '@file'
+			prefix: '@@',
+			basepath: '@file'
 		}))
 		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('mini-js', function() {
 	return gulp.src([
-			'./src/**/*.js',
-			'!./src/**/*.min.js'
-		])
+		'./src/**/*.js',
+		'!./src/**/*.min.js'
+	])
 		.pipe(babel({
 			presets: ['env']
 		}))
@@ -84,14 +84,15 @@ gulp.task('mini-sass', function() {
 		.pipe(gulp.dest('./dist/assets/css'));
 });
 
-gulp.task('debug', function() {
-	runSequence('clean', 'copy', 'html', 'js', 'sass', function() {
-		console.log('Finished');
-	});
-});
 
-gulp.task('default', ['debug'], function() {
-	gulp.watch('./src/**/*.html', ['debug']);
+gulp.task('default', ['clean', 'copy'], function() {
+	gulp.watch('./src/**/*.html', ['html']);
+	gulp.watch([
+		'./src/**/*.scss'
+		], ['sass']);
+	gulp.watch([
+		'./src/**/*.js'
+		], ['js']);
 });
 
 gulp.task('build', function() {
